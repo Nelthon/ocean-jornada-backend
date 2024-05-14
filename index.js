a8cf07267a5cbef9b6675e378943653fb9374981
Nelthon <nelthonnoronha@gmail.com>
2024-05-14 14:46:54 +0000

    desafio  /oi

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+//desafio: criar uma rota [GET] /oi que retorne "olá, mundo!"
 app.get('/oi', function (req, res){
   res.send("Olá Mundo!")
 })
+
 app.listen(3000)
